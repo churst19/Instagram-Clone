829ac16dfdc2517b42a21b61f3b1e87e0391c5f3
churst19 <churst19@gmail.com>
2022-10-21 18:22:21 -0400

    added like and unlike feature

--- server/routes/post.js
+++ server/routes/post.js
@@ -50,4 +50,34 @@ router.get('/mypost', requireLogin, (req,res)=>{
 })
 
 
+router.put('/like',requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id} //add user's like to array
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+
+router.put('/unlike',requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id} //remove user's like from array
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+
 module.exports = router
