755da32ccd0fc0b64aa9707aa315f6feb7219cb6
churst19 <churst19@gmail.com>
2022-10-21 21:20:38 -0400

    delete posts and comments

--- server/routes/post.js
+++ server/routes/post.js
@@ -102,4 +102,47 @@ router.put('/comment',requireLogin, (req, res) => {
     })
 })
 
+router.delete('/deletepost/:postId',requireLogin,(req,res)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy","_id")
+    .exec((err,post)=>{
+        if(err || !post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+            post.remove()
+            .then(result => {
+                res.json({result})
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    })
+})
+
+
+router.delete('/deletecomment/:postId/:commentId', requireLogin, (req, res) => {
+    Post.findById(req.params.postId)
+    //   .populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")
+    .exec((err,post)=>{
+        if(err || !post){
+            return res.status(422).json({message:"Some error occured!!"});
+        }
+        const comment = post.comments.find((comment)=>
+            comment._id.toString() === req.params.commentId.toString()
+            );
+            if (comment.postedBy._id.toString() === req.user._id.toString()) {
+                const removeIndex = post.comments
+                    .map(comment => comment._id.toString())
+                    .indexOf(req.params.commentId);
+                    post.comments.splice(removeIndex, 1);
+                    post.save()
+                    .then(result=>{
+                        res.json(result)
+                    }).catch(err=>console.log(err));
+            }
+    })
+});
+
 module.exports = router
