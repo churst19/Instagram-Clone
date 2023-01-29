d128f761ebf4ce19f4e33a2269c6cb77af01ea77
Chris <churst19@gmail.com>
2023-01-28 19:08:01 -0500

    path correction

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('../config/keys')
+const {MONGOURI} = require('./config/keys')
 // const dotenv = require('dotenv')
 // require('dotenv').config()
 const MONGOURI = process.env.MONGOURI
