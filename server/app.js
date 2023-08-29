8feaf222b9da229952e0ead7472b49613199bfc6
Chris <churst19@gmail.com>
2023-08-29 16:33:46 -0400

    heroku fix

--- server/app.js
+++ server/app.js
@@ -11,11 +11,11 @@ var cors = require("cors")
 app.use(cors())
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"))
-  const path = require("path")
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+  app.use(express.static("/client/build"))
+  // const path = require("path")
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  // })
 }
 
 mongoose.connect(MONGOURI, {
